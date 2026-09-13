66c0d7826bf2d75d4ad790116be18ab66cc18ac4
Soujash Mandal <2002soujash@gmail.com>
2026-09-14 01:45:40 +0530

    create tuple bug fix

--- PageService/actions/CreateTupleAction.cpp
+++ PageService/actions/CreateTupleAction.cpp
@@ -5,7 +5,6 @@ CreateTupleAction::CreateTupleAction(PagePort &pagePort) : pagePort(pagePort) {}
 
 void CreateTupleAction::execute(PageId pageId, const char *tupleData) {
   Page page;
-  pagePort.readPage(pageId, page);
   std::memcpy(page.data(), tupleData, Page::PAGE_SIZE);
   pagePort.writePage(pageId, page);
 }
