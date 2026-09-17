d951a43c018dde866da5c8989b9410eefcccfce5
Soujash Mandal <2002soujash@gmail.com>
2026-09-17 23:24:32 +0530

    Update FIFOEvictionPolicy.cpp

--- BufferPoolManager/domain/eviction/FIFOEvictionPolicy.cpp
+++ BufferPoolManager/domain/eviction/FIFOEvictionPolicy.cpp
@@ -4,7 +4,6 @@ void FIFOEvictionPolicy::RecordAccess(PageId pageId) {
   if (pagePositions_.contains(pageId)) {
     return;
   }
-
   pages_.push_back(pageId);
   pagePositions_[pageId] = std::prev(pages_.end());
 }
@@ -13,7 +12,6 @@ void FIFOEvictionPolicy::SetEvictable(PageId pageId, bool evictable) {
   if (!pagePositions_.contains(pageId)) {
     return;
   }
-
   if (evictable) {
     evictablePages_.insert(pageId);
   } else {
@@ -24,7 +22,6 @@ void FIFOEvictionPolicy::SetEvictable(PageId pageId, bool evictable) {
 std::optional<PageId> FIFOEvictionPolicy::Evict() {
   for (auto it = pages_.begin(); it != pages_.end(); ++it) {
     PageId pageId = *it;
-
     if (evictablePages_.contains(pageId)) {
       evictablePages_.erase(pageId);
       pagePositions_.erase(pageId);
@@ -32,17 +29,14 @@ std::optional<PageId> FIFOEvictionPolicy::Evict() {
       return pageId;
     }
   }
-
   return std::nullopt;
 }
 
 void FIFOEvictionPolicy::Remove(PageId pageId) {
   auto it = pagePositions_.find(pageId);
-
   if (it == pagePositions_.end()) {
     return;
   }
-
   evictablePages_.erase(pageId);
   pages_.erase(it->second);
   pagePositions_.erase(it);
