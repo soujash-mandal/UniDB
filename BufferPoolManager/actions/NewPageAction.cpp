6785dea30effc84e65795d0f3933a3a06b0efa89
Soujash Mandal <2002soujash@gmail.com>
2026-09-22 04:07:35 +0530

    NewPageAction

--- BufferPoolManager/actions/NewPageAction.cpp
+++ BufferPoolManager/actions/NewPageAction.cpp
@@ -1,20 +1,21 @@
 #include "NewPageAction.h"
 
+#include <optional>
 #include <stdexcept>
 
 NewPageAction::NewPageAction(BufferPool &bufferPool,
                              AllocatePagePort &allocatePagePort,
+                             WritePagePort &writePagePort,
                              EvictionPolicy &evictionPolicy)
     : bufferPool(bufferPool), allocatePagePort(allocatePagePort),
-      evictionPolicy(evictionPolicy) {}
+      writePagePort(writePagePort), evictionPolicy(evictionPolicy) {}
 
 Page &NewPageAction::execute() {
-  PageId pageId = allocatePagePort.allocatePage();
-
   // 1. First try to find an unused frame.
   for (uint32_t frameId = 0; frameId < bufferPool.size(); ++frameId) {
     Frame &frame = bufferPool.getFrame(frameId);
     if (!frame.isOccupied()) {
+      PageId pageId = allocatePagePort.allocatePage();
       frame.setPageId(pageId);
       frame.setOccupied(true);
       frame.setDirty(true);
@@ -25,25 +26,43 @@ Page &NewPageAction::execute() {
     }
   }
 
-  // 2. No free frame. Try to evict a page.
+  // 2. Buffer pool is full.
+  // Find a page that can be evicted.
   std::optional<PageId> victimPageId = evictionPolicy.Evict();
   if (!victimPageId.has_value()) {
     throw std::runtime_error("Buffer pool is full and no page is evictable");
   }
 
-  // 3. Find the frame containing the victim page.
+  // 3. Find the frame containing the victim.
   for (uint32_t frameId = 0; frameId < bufferPool.size(); ++frameId) {
     Frame &frame = bufferPool.getFrame(frameId);
-    if (frame.isOccupied() && frame.getPageId() == victimPageId.value()) {
-      frame.setPageId(pageId);
-      frame.setOccupied(true);
-      frame.setDirty(true);
-      frame.pin();
-      evictionPolicy.RecordAccess(pageId);
-      evictionPolicy.SetEvictable(pageId, false);
-      return frame.getPage();
+    if (!frame.isOccupied() || frame.getPageId() != victimPageId.value()) {
+      continue;
     }
+
+    // 4. Persist the victim if it is dirty.
+    if (frame.isDirty()) {
+      writePagePort.writePage(frame.getPageId(), frame.getPage());
+      frame.setDirty(false);
+    }
+
+    // 5. Allocate the new page only after we know
+    //    that the frame can actually be reused.
+    PageId pageId = allocatePagePort.allocatePage();
+
+    // 6. Replace the victim with the new page.
+    frame.setPageId(pageId);
+    frame.setOccupied(true);
+    frame.setDirty(true);
+    frame.pin();
+
+    // 7. Register the new page.
+    evictionPolicy.RecordAccess(pageId);
+    evictionPolicy.SetEvictable(pageId, false);
+
+    return frame.getPage();
   }
 
-  throw std::runtime_error("Evicted page frame not found");
+  throw std::runtime_error(
+      "Eviction policy returned a page not present in buffer pool");
 }
--- BufferPoolManager/actions/NewPageAction.h
+++ BufferPoolManager/actions/NewPageAction.h
@@ -4,17 +4,18 @@
 #include "../../core/Page.h"
 #include "../domain/BufferPool.h"
 #include "../port/AllocatePagePort.h"
+#include "../port/WritePagePort.h"
 
 class NewPageAction {
 public:
-  explicit NewPageAction(BufferPool &bufferPool,
-                         AllocatePagePort &allocatePagePort,
-                         EvictionPolicy &evictionPolicy);
+  NewPageAction(BufferPool &bufferPool, AllocatePagePort &allocatePagePort,
+                WritePagePort &writePagePort, EvictionPolicy &evictionPolicy);
 
   Page &execute();
 
 private:
   BufferPool &bufferPool;
   AllocatePagePort &allocatePagePort;
+  WritePagePort &writePagePort;
   EvictionPolicy &evictionPolicy;
 };
