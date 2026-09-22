ede077eb3a6b93aa406d8681b045071a2b81c294
Soujash Mandal <2002soujash@gmail.com>
2026-09-22 23:49:36 +0530

    Create FindPageWithSpaceAction.cpp

---
+++ FreeSpaceMapService/actions/FindPageWithSpaceAction.cpp
@@ -0,0 +1,39 @@
+#include "FindPageWithSpaceAction.h"
+
+#include "../domain/FreeSpaceMapPage.h"
+
+FindPageWithSpaceAction::FindPageWithSpaceAction(FetchPagePort &fetchPagePort,
+                                                 UnpinPagePort &unpinPagePort)
+    : fetchPagePort(fetchPagePort), unpinPagePort(unpinPagePort) {}
+
+PageId FindPageWithSpaceAction::execute(TableId tableId,
+                                        PageId firstFreeSpaceMapPageId,
+                                        uint32_t requiredSpace) {
+  PageId currentPageId = firstFreeSpaceMapPageId;
+
+  while (currentPageId != FreeSpaceMapPage::INVALID_PAGE_ID) {
+    Page &page = fetchPagePort.fetchPage(currentPageId);
+
+    FreeSpaceMapPage freeSpaceMapPage;
+    freeSpaceMapPage.readFromPage(page);
+
+    if (freeSpaceMapPage.getTableId() != tableId) {
+      unpinPagePort.unpinPage(currentPageId, false);
+      return FreeSpaceMapPage::INVALID_PAGE_ID;
+    }
+
+    PageId dataPageId = freeSpaceMapPage.findPageWithSpace(requiredSpace);
+
+    PageId nextPageId = freeSpaceMapPage.getNextPageId();
+
+    unpinPagePort.unpinPage(currentPageId, false);
+
+    if (dataPageId != FreeSpaceMapPage::INVALID_PAGE_ID) {
+      return dataPageId;
+    }
+
+    currentPageId = nextPageId;
+  }
+
+  return FreeSpaceMapPage::INVALID_PAGE_ID;
+}
